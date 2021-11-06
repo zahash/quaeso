c4a2ea88dca9a2845543c1242b8de31db471d1aa
zahash <zahash.z@gmail.com>
2021-11-06 13:33:20 +0530

    remove unnecessary return statements

--- yeet/yeet.py
+++ yeet/yeet.py
@@ -13,8 +13,8 @@ from pygments.formatters.terminal import TerminalFormatter
 
 
 def bout(x): sys.stdout.buffer.write(x)
-def out(x): return sys.stdout.write(str(x)+"\n")
-def err(x): return sys.stderr.write(str(x)+"\n")
+def out(x): sys.stdout.write(str(x)+"\n")
+def err(x): sys.stderr.write(str(x)+"\n")
 def by_key_lower(item: tuple): key, val = item; return key.lower()
 
 
